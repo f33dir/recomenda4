e1550e69e2a8a76f9f18e5ca31b90da95ab20471
f33dir <arseniy.tarasov@gmail.com>
2023-03-21 12:44:26 +0300

    added pg and express

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express();
-const port = 3002
+const port = 3003
 const pg = require('pg')
 const cs = "postgres://postgres:12345@localhost:5432/";
 const pgClient = new pg.Client(cs);
+app.use(bodyParser.json());
 pgClient.connect()
 
 pgClient.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
     console.log(err ? err.stack : res.rows[0].message) // Hello World!
     pgClient.end()
 })
-app.use(express.json);
 app.get('/',(req, res)=>{
-    res.send('hello fucker')
+    res.json('f u');
 })
 
 app.post('/register',(req, res) =>{
